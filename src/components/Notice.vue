48cbbe5618b3395dc5ddefbd3ed99e4c1dba64fa
jacksonPrimo <jackson.primo12@gmail.com>
2021-05-14 12:04:44 -0300

    create notice card

---
+++ src/components/Notice.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card">
+    <h2>{{title}}</h2>
+    <p>{{description}}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+<style scoped>
+.card {
+  text-align: center;
+  min-height: 30vh;
+  box-shadow: 5px 10px 2px 1px rgba(11, 11, 19, 0.2);
+}
+</style>
