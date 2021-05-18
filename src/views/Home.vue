13dc14c8f57661f6e3619d2559f431a0ceef0390
jacksonPrimo <jackson.primo12@gmail.com>
2021-05-18 12:23:31 -0300

    adjustment in responsivity

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
-  <b-container fluid class="d-flex flex-wrap pt-1">
-    <div class="col-3">
-      <Notice title="teste" description="apenas um teste"></Notice>
+  <b-container fluid>
+    <div v-if="noticeList.length" class="d-flex flex-wrap pt-1">
+      <div v-for="(notice, index) in noticeList" :key="index" class="col-lg-3 col-md-6 col-ls-1">
+        <Notice :title="notice.title" :description="notice.description"></Notice>
+      </div>
+    </div>
+    <div v-else class="d-flex flex-wrap pt-1">
+      <div class="col-lg-3 col-md-6 col-ls-1">
+        <Notice title="Sem Noticias" description="Não há notícias no momento"></Notice>
+      </div>
     </div>
   </b-container>
 </template>
@@ -14,6 +21,11 @@ export default {
   name: 'Home',
   components: {
     Notice
+  },
+  data(){
+    return {
+      noticeList: []
+    }
   },
   mounted(){
     axios.get('notices').then(
