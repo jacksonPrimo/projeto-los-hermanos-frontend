f8b5728f285e795dcb8b2f2ded6c3394dc7ce88c
jacksonPrimo <jackson.primo12@gmail.com>
2021-05-19 21:52:24 -0300

    finish user register

--- src/layout/LayoutDefault.vue
+++ src/layout/LayoutDefault.vue
@@ -5,26 +5,24 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav class="ml-auto">
         <b-navbar-nav class="ml-auto">
-          <b-nav-item >
-            <b-icon-arrow-clockwise class="icon"></b-icon-arrow-clockwise>
-            Atualizar
+          <b-nav-item v-if="isAdmin" to="/">
+            <b-icon-house-fill class="icon"></b-icon-house-fill>
+            Home
           </b-nav-item>
-          <b-nav-item to="Postagem">
+          <b-nav-item v-if="isAdmin" to="Postagens">
             <b-icon-plus class="icon"></b-icon-plus>
-              Criar
+            Suas Postagens
           </b-nav-item>
-          <b-nav-item >
-            <b-icon-share class="icon"></b-icon-share>
-              Compartilhar
-          </b-nav-item>
-          <template v-if="user">
-            <b-avatar></b-avatar>
+          <template v-if="authenticated">
             <b-nav-item-dropdown right >
-                <template #button-content>
-                  <em>{{ user.name }}</em>
-                </template>
-                <b-dropdown-item href="#">Profile</b-dropdown-item>
-                <b-dropdown-item @click="logout()">Sign Out</b-dropdown-item>
+              <template #button-content>
+                <em class="text-light">
+                  <b-icon-person-circle class="icon"></b-icon-person-circle>
+                  {{ userAuthenticated.email }}
+                </em>
+              </template>
+              <b-dropdown-item href="perfil">Profile</b-dropdown-item>
+              <b-dropdown-item @click="logout()">Sign Out</b-dropdown-item>
             </b-nav-item-dropdown>
           </template>
           <template v-else>
@@ -35,7 +33,7 @@
               <b-form v-if="!signupInModal" @submit.prevent="login()">
                 <b-form-group
                   id="input-group-1"
-                  label="Email address:"
+                  label="Email:"
                   label-for="email"
                 >
                   <b-form-input
@@ -47,11 +45,12 @@
                   ></b-form-input>
                 </b-form-group>
 
-                <b-form-group id="input-group-2" label="Your Name:" label-for="password">
+                <b-form-group id="input-group-2" label="Senha" label-for="password">
                   <b-form-input
                     id="password"
                     v-model="formSignin.password"
-                    placeholder="Enter name"
+                    placeholder="Sua senha"
+                    type="password"
                     required
                   ></b-form-input>
                 </b-form-group>
@@ -62,14 +61,14 @@
                   <b-link @click="signupInModal = !signupInModal">ainda não é um usuário? clique aqui para se cadastrar</b-link>
                 </div>
               </b-form>
-              <b-form v-if="signupInModal">
+              <b-form v-if="signupInModal" @submit.prevent="register()">
                 <b-form-group
                   label="Primeiro Nome"
                   label-for="firstName"
                 >
                   <b-form-input
                     id="firstName"
-                    v-model="formSignup.lastName"
+                    v-model="formSignup.firstName"
                     type="text"
                     placeholder="Primeiro Nome"
                     required
@@ -89,14 +88,14 @@
                 </b-form-group>
                 <b-form-group
                   id="input-group-1"
-                  label="Email address:"
+                  label="Email:"
                   label-for="email"
                 >
                   <b-form-input
                     id="email"
                     v-model="formSignup.email"
                     type="email"
-                    placeholder="Enter email"
+                    placeholder="Email"
                     required
                   ></b-form-input>
                 </b-form-group>
@@ -116,15 +115,15 @@
                     id="phone"
                     v-model="formSignup.phone"
                     placeholder="Telefone"
-                    required
                   ></b-form-input>
                 </b-form-group>
 
-                <b-form-group label="Your Name:" label-for="password">
+                <b-form-group label="Senha:" label-for="password">
                   <b-form-input
                     id="password"
                     v-model="formSignup.password"
-                    placeholder="Enter name"
+                    placeholder="Sua senha"
+                    type="password"
                     required
                   ></b-form-input>
                 </b-form-group>
@@ -133,11 +132,7 @@
                 </b-form-group>
                 <div class="text-center">
                   <b-link @click="signupInModal = !signupInModal">
-                    {{
-                      signupInModal
-                      ? 'ainda não é um usuário? clique aqui para se cadastrar'
-                      : 'ja é um usuário? clique aqui para fazer login'
-                    }}
+                    ja é um usuário? clique aqui para fazer login
                   </b-link>
                 </div>
               </b-form>
@@ -150,13 +145,12 @@
   </div>
 </template>
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 export default {
   name: 'LayoutDefalt',
   data(){
     return {
       signupInModal: false,
-      user: '',
       formSignin: {
         email: '',
         password: ''
@@ -172,20 +166,54 @@ export default {
       }
     }
   },
+  computed: {
+    ...mapGetters('auth', ['authenticated', 'userAuthenticated']),
+    isAdmin(){
+      if(this.authenticated && this.userAuthenticated.type == 'admin')
+        return true
+      else
+        return false
+    }
+  },
   methods: {
-    ...mapActions('auth', ['signIn', 'signUp']),
+    ...mapActions('auth', ['signIn', 'signUp', 'signOut']),
     closeModal(){
       this.$refs.loginModal.hide()
     },
     login(){
       this.signIn(this.formSignin).then(
-        resp=>{
-          console.log(resp)
+        ()=>{
           alert('usuário logado')
         },
         error=>{
-          alert('ocorreu um erro')
-          console.error(error)
+          const message = error.response.data.message
+          console.log({...error})
+          if(message == 'password incorrect')
+            alert('Senha Incorreta')
+          if(message == 'User with email not found')
+            alert('Usuário não encontrado')
+        }
+      )
+    },
+    logout(){
+      this.signOut()
+    },
+    register(){
+      console.log(this.formSignup)
+      this.signUp(this.formSignup).then(
+        () => {
+          alert('usuário cadastrado, agora faça login')
+          this.signupInModal = false
+        },
+        error=>{
+          console.log({...error})
+          const code = error.response.status
+          if(code === 409 )
+            alert('Este email ja está em uso')
+          if(code === 400 )
+            alert('Dados Invalidos')
+          if(code === 500 )
+            alert('Erro no servidor')
         }
       )
     }
